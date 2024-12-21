3aec83f384ea52320a3bca9329768201e7e9b35b
SKhairulin <skhairulin@m.ru>
2024-12-21 14:25:16 +0300

    fix pull

--- pull.bat
+++ pull.bat
@@ -1,9 +1,11 @@
 @echo off
 git pull
 cd lg_hak_back
+git restore .
 git pull
 cd ..
 cd lg_hak_front
+git restore .
 git pull
 cd ..
 echo Done
--- pull.sh
+++ pull.sh
@@ -4,11 +4,13 @@ git pull
 
 echo "Running git pull in lg_hak_back directory..."
 cd lg_hak_back
+git restore .
 git pull
 cd ..
 
 echo "Running git pull in lg_hak_front directory..."
 cd lg_hak_front
+git restore .
 git pull
 cd ..
 
